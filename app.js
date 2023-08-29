621b530e03a3e0d43e2078f708e3b96d54766dee
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-08-29 20:28:48 +0530

    Serve uploaded assets

--- app.js
+++ app.js
@@ -54,11 +54,22 @@ const multerFileFilter =
     console.log("File accepted");
     cb(null, true);
   };
-app.use(multer({ storage: multerConfig, fileFilter: multerFileFilter }).any());
+app.use(
+  multer({ storage: multerConfig, fileFilter: multerFileFilter }).single(
+    "myFile"
+  )
+);
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(require("cookie-parser")());
+
+// add multer-uploads getter
+app.get("/multer-uploads/:requestedFileName", (req, res, next) => {
+  res.sendFile(
+    path.join(__dirname, "multer-uploads", req.params.requestedFileName)
+  );
+});
 
 // learning flipper flags - to hide logs if they become too much
 app.use((req, res, next) => {
